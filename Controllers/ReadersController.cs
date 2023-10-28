c3997d527c9a77908587b744a47347197a8d6a90
Gheorghita Tudor <tudorgheorghita.vl@gmail.com>
2023-10-28 23:19:49 +0300

    (basic)Conntrollers and Views Added

---
+++ Controllers/ReadersController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AudioBooksApp.Data;
+using AudioBooksApp.Models;
+
+namespace AudioBooksApp.Controllers
+{
+    public class ReadersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ReadersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Readers
+        public async Task<IActionResult> Index()
+        {
+              return _context.Readers != null ? 
+                          View(await _context.Readers.ToListAsync()) :
+                          Problem("Entity set 'AppDbContext.Readers'  is null.");
+        }
+
+        // GET: Readers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Readers == null)
+            {
+                return NotFound();
+            }
+
+            var reader = await _context.Readers
+                .FirstOrDefaultAsync(m => m.ReaderId == id);
+            if (reader == null)
+            {
+                return NotFound();
+            }
+
+            return View(reader);
+        }
+
+        // GET: Readers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Readers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ReaderId,Name,Biography")] Reader reader)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(reader);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(reader);
+        }
+
+        // GET: Readers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Readers == null)
+            {
+                return NotFound();
+            }
+
+            var reader = await _context.Readers.FindAsync(id);
+            if (reader == null)
+            {
+                return NotFound();
+            }
+            return View(reader);
+        }
+
+        // POST: Readers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ReaderId,Name,Biography")] Reader reader)
+        {
+            if (id != reader.ReaderId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reader);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReaderExists(reader.ReaderId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(reader);
+        }
+
+        // GET: Readers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Readers == null)
+            {
+                return NotFound();
+            }
+
+            var reader = await _context.Readers
+                .FirstOrDefaultAsync(m => m.ReaderId == id);
+            if (reader == null)
+            {
+                return NotFound();
+            }
+
+            return View(reader);
+        }
+
+        // POST: Readers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Readers == null)
+            {
+                return Problem("Entity set 'AppDbContext.Readers'  is null.");
+            }
+            var reader = await _context.Readers.FindAsync(id);
+            if (reader != null)
+            {
+                _context.Readers.Remove(reader);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReaderExists(int id)
+        {
+          return (_context.Readers?.Any(e => e.ReaderId == id)).GetValueOrDefault();
+        }
+    }
+}
