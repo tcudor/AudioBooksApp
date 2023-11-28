1397bf78b7da4e4cb3163d3d3426fec14a8054c8
Gheorghita Tudor <tudorgheorghita.vl@gmail.com>
2023-11-28 12:59:27 +0200

    audiobook.docx added

--- Data/Base/IEntityBaseRepository.cs
+++ Data/Base/IEntityBaseRepository.cs
@@ -5,7 +5,6 @@ namespace AudioBooksApp.Data.Base
     public interface IEntityBaseRepository<T> where T:class,IEntityBase,new()
     {
         Task<IEnumerable<T>> GetAllAsync();
-        //Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties);
         Task<T> GetByIdAsync(int id);
         Task AddAsync(T entity);
         Task UpdateAsync(int id, T entity);
